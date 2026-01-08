29b125f08076e9ef7d44b82ebff9d75aed960699
0xshoegazer <0xshoegazer@gmail.com>
2026-01-08 14:37:24 -0500

    setup apollo gql client

--- components/ui/provider.tsx
+++ components/ui/provider.tsx
@@ -7,19 +7,29 @@ import { WagmiProvider } from 'wagmi';
 import { wagmiConfig } from '@/lib/wagmi-config';
 import { CurrentUserDataProvider } from '@/lib/hooks/useCurrentUser';
 import { SelectedChainProvider } from '@/lib/hooks/useSelectedChain';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
 
 const queryClient = new QueryClient();
+
+// Dummy client to give to provider. Instances are chain specific at query time
+const gqlClient = new ApolloClient({
+  link: new HttpLink({ uri: 'https://flyby-router-demo.herokuapp.com/' }),
+  cache: new InMemoryCache(),
+});
 
 export function Provider(props: ColorModeProviderProps) {
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         <ChakraProvider value={defaultSystem}>
-          <SelectedChainProvider>
-            <CurrentUserDataProvider>
-              <ColorModeProvider {...props} />
-            </CurrentUserDataProvider>
-          </SelectedChainProvider>
+          <ApolloProvider client={gqlClient}>
+            <SelectedChainProvider>
+              <CurrentUserDataProvider>
+                <ColorModeProvider {...props} />
+              </CurrentUserDataProvider>
+            </SelectedChainProvider>
+          </ApolloProvider>
         </ChakraProvider>
       </QueryClientProvider>
     </WagmiProvider>
