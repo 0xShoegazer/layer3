8b43c2f265fd5309cc6098a66674a95531e0fb50
0xshoegazer <0xshoegazer@gmail.com>
2026-01-10 14:33:24 -0500

    style updates

--- components/user/UserCard.tsx
+++ components/user/UserCard.tsx
@@ -5,18 +5,20 @@ import { Card, Flex, Text } from '@chakra-ui/react';
 export default function UserCard({ user }: { user: Layer3User }) {
   return (
     <Card.Root>
-      <Card.Header fontWeight={'bold'} fontSize={'1.1rem'}>
-        {user.username} ({user.address})
+      <Card.Header fontWeight={'bold'}>
+        <Flex justifyContent={'space-between'}>
+          <Text fontSize={'1.1rem'}> {user.username}</Text>
+          <Text>{user.address}</Text>
+        </Flex>
       </Card.Header>
       <Card.Body>
-        <Flex justifyContent={'space-evenly'} gap={5}>
+        <Flex justifyContent={'space-between'} gap={5}>
           <Text>Layer3 Rank: {user.rank}</Text>
           <Text>Layer3 Level: {user.level}</Text>
           <Text>Layer3 XP: {user.xp}</Text>
           {/* <ChainIconsBanner gap={10} /> */}
         </Flex>
       </Card.Body>
-      <Card.Footer />
     </Card.Root>
   );
 }
